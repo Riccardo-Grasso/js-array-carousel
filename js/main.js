8e37809ddadaa043f9ff74652576b79ab8e02de1
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-14 21:03:12 +0200

    for

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ const text = [
 ];
 
 //SELEZIONO LA COLONNA CON IMG GRANDE
-const imageBig = document.querySelector(".row .image-big");
+const imageBig = document.querySelector(".image-big");
 imageBig.classList.add("image-big-container");
 
 //INIZIALIZZO INDICE
@@ -43,3 +43,63 @@ for (let i = 0; i < items.length; i++) {
     const img = `<img class="${classeActive}" src="${currentImg}" alt="immagine ${i}">`;
     imageBig.innerHTML += img;
 }
+
+//LISTA IMG DESTRA
+const imageList = document.querySelector(".row .image-list");
+imageList.classList.add("image-list-container", "d-flex", "flex-column");
+
+for (let i = 0; i < items.length; i++) {
+    const currentImg = items[i];
+
+    let classeActive = "";
+
+    if (i === currentIndex) {
+        classeActive = "active";
+    }
+
+    const img = `<img class="${classeActive}" src="${currentImg}" alt="immagine ${i}">`;
+    imageList.innerHTML += img;
+}
+
+//FRECCE 
+const arrowTop = document.querySelector(".arrow-top");
+const arrowBottom = document.querySelector(".arrow-bottom");
+
+arrowBottom.addEventListener("click", function () {
+    const imgActiveRight = imageBig.querySelector(".active");
+    const imgActive = imageList.querySelector(".active");
+    imgActive.classList.remove("active");
+    imgActiveRight.classList.remove("active");
+    currentIndex++;
+
+    if (currentIndex > items.length - 1) {
+        currentIndex = 0;
+    }
+
+    const imgTags = imageList.querySelectorAll("img");
+    const imgTagsBig = imageBig.querySelectorAll("img");
+    const newActiveImage = imgTags[currentIndex];
+    const newActiveImageBig = imgTagsBig[currentIndex];
+    newActiveImage.classList.add("active");
+    newActiveImageBig.classList.add("active");
+});
+
+arrowTop.addEventListener("click", function () {
+
+    const imgActiveRight = imageBig.querySelector(".active");
+    const imgActive = imageList.querySelector(".active");
+    imgActive.classList.remove("active");
+    imgActiveRight.classList.remove("active");
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = items.length - 1;
+    }
+
+    const imgTags = imageList.querySelectorAll("img");
+    const imgTagsBig = imageBig.querySelectorAll("img");
+    const newActiveImage = imgTags[currentIndex];
+    const newActiveImageBig = imgTagsBig[currentIndex];
+    newActiveImage.classList.add("active");
+    newActiveImageBig.classList.add("active");
+});
